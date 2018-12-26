9936620efa8c492500aba3705f5475f35e0ece33
Shekcon <quangsang9773@gmail.com>
2018-12-26 15:10:54 +0700

    SQL Avdanced

--- 07.sql
+++ 07.sql
@@ -1,7 +1,7 @@
-SELECT teams.name, statistics.tactics
+SELECT teams.name
 FROM teams
     INNER JOIN statistics ON teams.id = statistics.team_id
--- GROUP BY teams.name
--- HAVING count(teams.name) > 1
+GROUP BY teams.name
+HAVING count(DISTINCT statistics.tactics) > 1
 ORDER BY teams.name
 ;
